2e63b2f208f4c0cd35d858c7b9f4b991d26db7b5
Brice Maltby <b@bricemaltby.com>
2023-01-03 19:29:23 -0600

    Basic CLI arg handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{App,Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let args = App::new("rlox")
+        .arg(Arg::with_name("filepath")
+             .takes_value(true))
+        .get_matches();
+    
+    match args.value_of("filepath") {
+        Some(filepath) => {
+            run_file(filepath);
+        },
+        None => {
+            run_prompt();
+        }
+    }
+}
+
+fn run_file(filepath: &str) {
+    print!("{:}", filepath);
+}
+
+fn run_prompt() {
+    print!("runPrompt()");
 }
