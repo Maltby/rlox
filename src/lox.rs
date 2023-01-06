e5c77760ab47ea19c8bdb0b431db15e6b169b835
Brice Maltby <b@bricemaltby.com>
2023-01-06 07:29:44 -0600

    Organize some stuff

--- src/lox.rs
+++ src/lox.rs
@@ -1,12 +1,13 @@
 use clap::{App,Arg};
 use std::fs;
 use std::io;
+use std::process;
 
 pub struct Lox {
     pub had_error: bool
 }
 impl Lox {
-    pub fn main(&self) {
+    pub fn main(&mut self) {
         let args = App::new("rlox")
             .arg(Arg::with_name("filepath")
                  .takes_value(true))
@@ -14,25 +15,43 @@ impl Lox {
 
         match args.value_of("filepath") {
             Some(filepath) => {
-                run_file(filepath);
+                self.run_file(filepath);
             },
             None => {
-                run_prompt();
+                self.run_prompt();
             }
         }
     }
-}
 
-fn run_file(filepath: &str) {
-    let contents = fs::read_to_string(filepath)
-        .expect(&format!("Failed to read from given filepath: {:?}", filepath));
-    run(contents);
-}
+    fn run_file(&self, filepath: &str) {
+        let contents = fs::read_to_string(filepath)
+            .expect(&format!("Failed to read from given filepath: {:?}", filepath));
+        Lox::run(contents);
+        if self.had_error {process::exit(65);}
+    }
 
-fn run_prompt() {
-    let lines = io::stdin().lines();
-    for line in lines {
-        run(line.unwrap());
+    fn run_prompt(&mut self) {
+        let lines = io::stdin().lines();
+        for line in lines {
+            Lox::run(line.unwrap());
+            self.had_error = false;
+        }
+    }
+
+    fn run(source: String) {
+        let mut scanner: Scanner = Scanner {source};
+        let tokens = scanner.scan_tokens();
+        for token in tokens {
+            println!("token: {token}");
+        }
+    }
+
+    fn error(line: usize, message: &str) {
+        Lox::report(line, "", message);
+    }
+
+    fn report(line: usize, _where: &str, message: &str) {
+        println!("[line {line}] Error{_where}: {message}");
     }
 }
 
@@ -47,21 +66,4 @@ impl Scanner {
         }
         tokens
     }
-}
-
-fn run(source: String) {
-    let mut scanner: Scanner = Scanner {source};
-    let tokens = scanner.scan_tokens();
-    for token in tokens {
-        println!("token: {token}");
-    }
-
-}
-
-fn error(line: usize, message: &str) {
-    report(line, "", message);
-}
-
-fn report(line: usize, _where: &str, message: &str) {
-    println!("[line {line}] Error{_where}: {message}");
 }
