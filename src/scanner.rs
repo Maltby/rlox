71f650a2de26614b69d313de3f0d5bf45a662e1e
Brice Maltby <b@bricemaltby.com>
2023-01-10 17:10:10 -0600

    Handle Slash token

--- src/scanner.rs
+++ src/scanner.rs
@@ -5,6 +5,7 @@ pub struct Scanner {
     view: String, // Current char(s) being scanned
     chars: std::iter::Peekable<std::vec::IntoIter<char>>,
     pub tokens: Vec<Token>,
+    pub errors: Vec<lox::Error>,
     start: usize,
     current: usize,
     line: usize,
@@ -25,15 +26,16 @@ impl Scanner {
             let c = scanner.chars.next().unwrap();
             scanner.start = scanner.current;
             scanner.view.push(c);
-            match scanner.scan_token() {
-                Ok(token) => {
-                    scanner.tokens.push(token);
-                    scanner.view = "".to_string();
-                }
-                Err(e) => {
-                    errors.push(e);
-                }
-            }
+            scanner.scan_token();
+            // match scanner.scan_token() {
+            //     Ok(token) => {
+            //         scanner.tokens.push(token);
+            //         scanner.view = "".to_string();
+            //     }
+            //     Err(e) => {
+            //         errors.push(e);
+            //     }
+            // }
         }
         scanner.tokens.push(
             Token { 
@@ -49,60 +51,73 @@ impl Scanner {
         Err(errors)
     }
 
-    fn scan_token(&mut self) -> Result<Token, lox::Error> {
+    fn scan_token(&mut self) {
         match self.view.as_str() {
-            "(" => Ok(Self::create_token(TokenType::LeftParen, &mut self.view, self.line)),
-            ")" => Ok(Self::create_token(TokenType::RightParen, &mut self.view, self.line)),
-            "{" => Ok(Self::create_token(TokenType::LeftBrace, &mut self.view, self.line)),
-            "}" => Ok(Self::create_token(TokenType::RightBrace, &mut self.view, self.line)),
-            "," => Ok(Self::create_token(TokenType::Comma, &mut self.view, self.line)),
-            "." => Ok(Self::create_token(TokenType::Dot, &mut self.view, self.line)),
-            "-" => Ok(Self::create_token(TokenType::Minus, &mut self.view, self.line)),
-            "+" => Ok(Self::create_token(TokenType::Plus, &mut self.view, self.line)),
-            ";" => Ok(Self::create_token(TokenType::Semicolon, &mut self.view, self.line)),
-            "*" => Ok(Self::create_token(TokenType::Star, &mut self.view, self.line)),
+            "(" => self.tokens.push(Self::create_token(TokenType::LeftParen, &mut self.view, self.line)),
+            ")" => self.tokens.push(Self::create_token(TokenType::RightParen, &mut self.view, self.line)),
+            "{" => self.tokens.push(Self::create_token(TokenType::LeftBrace, &mut self.view, self.line)),
+            "}" => self.tokens.push(Self::create_token(TokenType::RightBrace, &mut self.view, self.line)),
+            "," => self.tokens.push(Self::create_token(TokenType::Comma, &mut self.view, self.line)),
+            "." => self.tokens.push(Self::create_token(TokenType::Dot, &mut self.view, self.line)),
+            "-" => self.tokens.push(Self::create_token(TokenType::Minus, &mut self.view, self.line)),
+            "+" => self.tokens.push(Self::create_token(TokenType::Plus, &mut self.view, self.line)),
+            ";" => self.tokens.push(Self::create_token(TokenType::Semicolon, &mut self.view, self.line)),
+            "*" => self.tokens.push(Self::create_token(TokenType::Star, &mut self.view, self.line)),
             "!" => {
                 match self.chars.peek() {
                     Some('=') => {
                         self.chars.next();
-                        Ok(Self::create_token(TokenType::BangEqual, &mut self.view, self.line))
+                        self.tokens.push(Self::create_token(TokenType::BangEqual, &mut self.view, self.line))
                     },
-                    _ => Ok(Self::create_token(TokenType::Bang, &mut self.view, self.line))
+                    _ => self.tokens.push(Self::create_token(TokenType::Bang, &mut self.view, self.line))
                 }
             }
             "=" => {
                 match self.chars.peek() {
                     Some('=') => {
                         self.chars.next();
-                        Ok(Self::create_token(TokenType::EqualEqual, &mut self.view, self.line))
+                        self.tokens.push(Self::create_token(TokenType::EqualEqual, &mut self.view, self.line))
                     },
-                    _ => Ok(Self::create_token(TokenType::Equal, &mut self.view, self.line))
+                    _ => self.tokens.push(Self::create_token(TokenType::Equal, &mut self.view, self.line))
                 }
             }
             "<" => {
                 match self.chars.peek() {
                     Some('=') => {
                         self.chars.next();
-                        Ok(Self::create_token(TokenType::LessEqual, &mut self.view, self.line))
+                        self.tokens.push(Self::create_token(TokenType::LessEqual, &mut self.view, self.line))
                     },
-                    _ => Ok(Self::create_token(TokenType::Less, &mut self.view, self.line))
+                    _ => self.tokens.push(Self::create_token(TokenType::Less, &mut self.view, self.line))
                 }
             }
             ">" => {
                 match self.chars.peek() {
                     Some('=') => {
                         self.chars.next();
-                        Ok(Self::create_token(TokenType::GreaterEqual, &mut self.view, self.line))
+                        self.tokens.push(Self::create_token(TokenType::GreaterEqual, &mut self.view, self.line))
                     },
-                    _ => Ok(Self::create_token(TokenType::Greater, &mut self.view, self.line))
+                    _ => self.tokens.push(Self::create_token(TokenType::Greater, &mut self.view, self.line))
                 }
             }
-            _ => Err(
-                lox::Error {
-                    line: self.line,
-                    message: "Unexpected character.".to_string()
+            "/" => {
+                match self.chars.peek() {
+                    Some('/') => {
+                        self.chars.next();
+                        while self.chars.peek() != Some(&'\n') && self.chars.peek() != None {
+                            self.chars.next();
+                        };
+                    },
+                    _ => self.tokens.push(Self::create_token(TokenType::Slash, &mut self.view, self.line))
                 }
-                )
+            }
+            _ => {
+                self.errors.push(
+                    lox::Error {
+                        line: self.line,
+                        message: "Unexpected character.".to_string()
+                    }
+                    );
+            }
         }
     }
 
@@ -126,6 +141,7 @@ impl Default for Scanner {
             view: "".to_string(),
             chars: "".to_string().chars().collect::<Vec<_>>().into_iter().peekable(),
             tokens: vec!(),
+            errors: vec!(),
             start: 0,
             current: 0,
             line: 1,
